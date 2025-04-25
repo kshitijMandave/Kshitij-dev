310030841ee82e46a84e7e2a78fb404bb95203c2
kshitijMandave <kshitij.mandwe@gmail.com>
2025-04-25 15:17:03 +0530

    added resume download feature

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import kshitijProfile from "../../images/Kshitij-Img.webp"
+import kshitijResume from "../../components/Hero/Kshitij_Mandave.pdf"
 
 function Hero() {
   return (
@@ -16,14 +17,15 @@ function Hero() {
               I’m Kshitij, a passionate MERN Stack Engineer with a background from BCA. I specialize in building dynamic and responsive web applications using MongoDB, Express.js, React, and Node.js. My experience spans end-to-end development, ensuring seamless integration and optimal performance. I thrive on creating innovative solutions and enhancing user experiences.
             </p>
             <a
-              href="/resume.pdf"
-              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
-            >
+              href={kshitijResume}
+              download
+              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
               Download Resume
             </a>
+
           </div>
         </div>
-        
+
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:p-8">
             <img
